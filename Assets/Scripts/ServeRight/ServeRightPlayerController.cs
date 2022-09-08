5ce456bf14aeed4a85365f0e3e455de87e14f66c
Rasmus Skovbo <rasmus.skovbo@shape.dk>
2022-09-08 16:27:59 +0200

    Updated anchor points moving

--- Assets/Scripts/ServeRight/ServeRightPlayerController.cs
+++ Assets/Scripts/ServeRight/ServeRightPlayerController.cs
@@ -32,49 +32,76 @@ public class ServeRightPlayerController : MonoBehaviour
     [Header("Snapping Movement Settings")]
     [SerializeField] float xMovementDeadzone = 0.25f;
     [SerializeField] float yMovementDeadzone = 0.25f;
-    [SerializeField] float xCoordOffset = 1.5f;
-    [SerializeField] float yCoordOffset = 3;
+    [SerializeField] float booths_xOffset = 1.5f;
+    [SerializeField] float trashcans_xOffset = 1f;
+    [SerializeField] float aisles_xOffset = 1.75f;
+    [SerializeField] float yAnchorsDistance = 1f;
     [SerializeField] float changePositionCooldownMS = 100;
     [SerializeField] bool canChangePositions = true;
-    Vector2 topleftCoord;
-    Vector2 topMidCoord;
-    Vector2 topRightCoord;
-    Vector2 leftCoord;
-    Vector2 centerCoord;
-    Vector2 rightCoord;
-    Vector2 bottomLeftCoord;
-    Vector2 bottomMidCoord;
-    Vector2 bottomRightCoord;
+
+    // Movement anchors
+    Dictionary<Vector2, xCoordType> centerCoordsDict;
+    List<Vector2> centerCoordsList;
+    Vector2 topCenterCoord;
+    Vector2 aisleOne_CenterCoord;
+    Vector2 trashCanTop_CenterCoord;
+    Vector2 aisleTwo_CenterCoord;
+    Vector2 aisleThree_CenterCoord;
+    Vector2 trashCanBot_CenterCoord;
+    Vector2 aisleFour_CenterCoord;
+    Vector2 botCenterCoord;
+
+    Vector2 defaultCoords = new Vector2(20,20);
+
+    private enum xCoordType {
+        Booth,
+        Aisle,
+        Trashcan
+    }
 
     private void Start() 
     {
         body = GetComponent<Rigidbody2D>();
 
-        topleftCoord = new Vector2(-xCoordOffset, yCoordOffset);
-        topMidCoord = new Vector2(0, yCoordOffset);
-        topRightCoord = new Vector2(xCoordOffset, yCoordOffset);
+        topCenterCoord = new Vector2(0, yAnchorsDistance * 3);
+        aisleOne_CenterCoord = new Vector2(0, yAnchorsDistance * 2.2f);
+        trashCanTop_CenterCoord = new Vector2(0, yAnchorsDistance * 1.5f);
+        aisleTwo_CenterCoord = new Vector2(0, yAnchorsDistance * 0.8f);
 
-        leftCoord = new Vector2(-xCoordOffset, 0);
-        centerCoord = new Vector2(0, 0);
-        rightCoord = new Vector2(xCoordOffset, 0);
+        aisleThree_CenterCoord = new Vector2(0, yAnchorsDistance * -0.8f);
+        trashCanBot_CenterCoord = new Vector2(0, yAnchorsDistance * -1.5f);
+        aisleFour_CenterCoord = new Vector2(0, yAnchorsDistance * -2.2f);
+        botCenterCoord = new Vector2(0, yAnchorsDistance * -3);
 
-        bottomLeftCoord = new Vector2(-xCoordOffset, -yCoordOffset);
-        bottomMidCoord = new Vector2(0, -yCoordOffset);
-        bottomRightCoord = new Vector2(xCoordOffset, -yCoordOffset);
+        centerCoordsDict = new Dictionary<Vector2, xCoordType> {
+            { topCenterCoord, xCoordType.Booth },
+            { aisleOne_CenterCoord, xCoordType.Aisle },
+            { trashCanTop_CenterCoord, xCoordType.Trashcan },
+            { aisleTwo_CenterCoord, xCoordType.Aisle },
+            { aisleThree_CenterCoord, xCoordType.Aisle },
+            { trashCanBot_CenterCoord, xCoordType.Trashcan },
+            { aisleFour_CenterCoord, xCoordType.Aisle },
+            { botCenterCoord, xCoordType.Booth }
+        };
 
-    }
+        centerCoordsList = new List<Vector2> {
+            topCenterCoord,
+            aisleOne_CenterCoord,
+            trashCanTop_CenterCoord,
+            aisleTwo_CenterCoord,
+            aisleThree_CenterCoord,
+            trashCanBot_CenterCoord,
+            aisleFour_CenterCoord,
+            botCenterCoord
+        };
 
-    void Update()
-    {
-        // Limit movement to border of x + y here
-        transform.position = new Vector2(
-            Mathf.Clamp (transform.position.x, -2, 2), 
-            Mathf.Clamp(transform.position.y, -4.5f, 4.5f) 
-        );
+        transform.localPosition = topCenterCoord;
     }
 
     private void FixedUpdate() 
     {
+        UpdateWithSnappingMovement();
+        /*
         switch (movementType) 
         {
             case MovementType.Relative:
@@ -87,9 +114,10 @@ public class ServeRightPlayerController : MonoBehaviour
                 UpdateWithAbsoluteMovement();
                 break;
         }
+        */
     }
 
-    /*
+    
     protected void OnGUI()
     {
         GUI.skin.label.fontSize = Screen.width / 20;
@@ -98,13 +126,146 @@ public class ServeRightPlayerController : MonoBehaviour
         GUILayout.Label("iphone width: " + Screen.width + " : " + GUI.skin.label.fontSize);
         GUILayout.Label("Input X: " + Input.acceleration.x);
         GUILayout.Label("Input Y: " + Input.acceleration.y);
-        GUILayout.Label("Target position X: " + dirX);
-        GUILayout.Label("Target position Y: " + dirY);
         GUILayout.Label("Transform X: " + transform.localPosition.x);
         GUILayout.Label("Transform Y: " + transform.localPosition.y);
-        GUILayout.Label("Absolute difference: " + (transform.localPosition.x - dirX));
     }
-    */
+    
+
+    void UpdateWithSnappingMovement()
+    {
+        // Cache phone accelerometer input & position
+        var inputX = Input.acceleration.x;
+        var inputY = Input.acceleration.y + verticalTiltOffset;
+
+        var currentPosition = transform.localPosition;
+
+        // Update target positions based on tilt of phone.
+        var targetPosition = new Vector2(-10, -10);
+    
+        // Moving up.
+        if (inputY > yMovementDeadzone) {
+            //Debug.Log("Attempting to move up");
+            // if x != 0 we're in a side position and cannot move up -> do nothing.
+            if (currentPosition.x != 0) return;
+
+            // If we're at the top, we cannot move further up -> do nothing.
+            if (currentPosition.y == topCenterCoord.y) return;
+
+    
+            var positionIndex = centerCoordsList.FindIndex(coord => coord.y == currentPosition.y);
+
+            targetPosition = new Vector2(currentPosition.x, centerCoordsList[positionIndex - 1].y);
+
+            UpdatePosition(targetPosition);
+        }
+
+        // mMving down
+        if (inputY < -yMovementDeadzone) {
+
+            // if x != 0 we're in a side position and cannot move up -> do nothing.
+            if (currentPosition.x != 0) return;
+
+            // If we're at the bottom, we cannot move further down -> do nothing.
+            if (currentPosition.y == botCenterCoord.y) return;
+            
+            var positionIndex = centerCoordsList.FindIndex(coord => coord.y == currentPosition.y);
+
+            targetPosition = new Vector2(currentPosition.x, centerCoordsList[positionIndex + 1].y);
+
+            UpdatePosition(targetPosition);
+        }
+
+        // Moving right ->
+        if (inputX > xMovementDeadzone) {
+
+            // If we're in a position to the right -> do nothing
+            if (currentPosition.x > 0) return;
+
+            
+            Vector2 dictLookup = defaultCoords;
+
+            foreach (Vector2 coord in centerCoordsDict.Keys) {
+                if (coord.Equals(currentPosition)) {
+                    dictLookup = coord;
+                }
+            }
+
+            // If true, means we're not in the middle -> We need to move to middle
+            // If we found a coord, means we're in the middle. Use offsets to move to the right
+            if (dictLookup.Equals(defaultCoords)) {
+                targetPosition = new Vector2(0, currentPosition.y);
+            } else {
+                xCoordType currentRowType = centerCoordsDict[dictLookup];
+
+                switch (currentRowType) 
+                {
+                    case xCoordType.Booth:
+                        targetPosition = new Vector2(currentPosition.x + booths_xOffset, currentPosition.y);
+                        break;
+                    case xCoordType.Trashcan:
+                        targetPosition = new Vector2(currentPosition.x + trashcans_xOffset, currentPosition.y);
+                        break;
+                    default:
+                        targetPosition = new Vector2(currentPosition.x + aisles_xOffset, currentPosition.y);
+                        break;
+                }
+            }
+
+            UpdatePosition(targetPosition);
+                
+        } 
+
+        // Moving left ->
+        if (inputX < -xMovementDeadzone) {
+            // If we're in a position to the left -> do nothing
+            if (currentPosition.x < 0) return;
+
+            Vector2 dictLookup = defaultCoords;
+
+            foreach (Vector2 coord in centerCoordsDict.Keys) {
+                if (coord.Equals(currentPosition)) {
+                    dictLookup = coord;
+                }
+            }
+
+            // If true, means we're not in the middle -> We need to move to middle
+            // If we found a coord, means we're in the middle. Use offsets to move to the left
+            if (dictLookup.Equals(defaultCoords)) {
+                targetPosition = new Vector2(0, currentPosition.y);
+            } else {
+                xCoordType currentRowType = centerCoordsDict[dictLookup];
+
+                switch (currentRowType) 
+                {
+                    case xCoordType.Booth:
+                        targetPosition = new Vector2(currentPosition.x - booths_xOffset, currentPosition.y);
+                        break;
+                    case xCoordType.Trashcan:
+                        targetPosition = new Vector2(currentPosition.x - trashcans_xOffset, currentPosition.y);
+                        break;
+                    default:
+                        targetPosition = new Vector2(currentPosition.x - aisles_xOffset, currentPosition.y);
+                        break;
+                }
+            }
+
+            UpdatePosition(targetPosition);
+        } 
+        
+    }
+
+    IEnumerator StartPositionChangeCooldown() {
+        canChangePositions = false;
+        yield return new WaitForSecondsRealtime(changePositionCooldownMS / 1000);
+        canChangePositions = true;
+    }
+
+    void UpdatePosition(Vector2 targetPosition) {
+        if (!transform.localPosition.Equals(targetPosition) && canChangePositions) {
+            transform.localPosition = targetPosition;
+            StartCoroutine(StartPositionChangeCooldown());
+        }
+    }
 
     void UpdateWithAbsoluteMovement() 
     {
@@ -160,72 +321,6 @@ public class ServeRightPlayerController : MonoBehaviour
         || (Mathf.Abs(targetPosition.y - transform.localPosition.y) > jitterBuffer)  ) {
             transform.localPosition = Vector2.Lerp(transform.localPosition, targetPosition, Time.deltaTime * updateSpeed);
         }
-    }
 
-    void UpdateWithSnappingMovement()
-    {
-        // TODO use tuples for the different approved positions.
-        // Possibly, depending on world x.y,  positions should be dynamic, based on screensize.
-
-        // Cache phone accelerometer input & position
-        var inputX = Input.acceleration.x;
-        var inputY = Input.acceleration.y + verticalTiltOffset;
-        dirY = Input.acceleration.y + verticalTiltOffset;
-        var localPosition = transform.localPosition;
-
-        // Update target positions based on tilt of phone.
-        var targetPosition = new Vector2();
-    
-        // Top
-        if (inputY > yMovementDeadzone) {
-            bool canMoveToTopCorners = localPosition.Equals(topMidCoord);
-            bool isCurrentlyOnSidePosition = localPosition.Equals(topleftCoord) || localPosition.Equals(topRightCoord);
-
-            if (inputX > xMovementDeadzone && (canMoveToTopCorners || isCurrentlyOnSidePosition)) {
-                targetPosition = topRightCoord;
-            } else if (inputX < -xMovementDeadzone && canMoveToTopCorners) {
-                targetPosition = topleftCoord;
-            } else {
-                targetPosition = topMidCoord;
-            }
-        }
-        // Center
-        if (inputY > -yMovementDeadzone && inputY < yMovementDeadzone ) {
-            bool canMoveToCenterSides = localPosition.Equals(centerCoord);
-            bool isCurrentlyOnSidePosition = localPosition.Equals(leftCoord) || localPosition.Equals(rightCoord);
-
-            if (inputX > xMovementDeadzone && (canMoveToCenterSides || isCurrentlyOnSidePosition)) {
-                targetPosition = rightCoord;
-            } else if (inputX < -xMovementDeadzone && (canMoveToCenterSides || isCurrentlyOnSidePosition)) {
-                targetPosition = leftCoord;
-            } else {
-                targetPosition = centerCoord;
-            }
-        }
-        // Bottom
-        if (inputY < -yMovementDeadzone) {
-            bool canMoveToBottomCorners = localPosition.Equals(bottomMidCoord);
-            bool isCurrentlyOnSidePosition = localPosition.Equals(bottomLeftCoord) || localPosition.Equals(bottomRightCoord);
-
-            if (inputX > xMovementDeadzone && (canMoveToBottomCorners || isCurrentlyOnSidePosition)) {
-                targetPosition = bottomRightCoord;
-            } else if (inputX < -xMovementDeadzone && canMoveToBottomCorners) {
-                targetPosition = bottomLeftCoord;
-            } else {
-                targetPosition = bottomMidCoord;
-            }
-        }
-
-        if (!transform.localPosition.Equals(targetPosition) && canChangePositions) {
-            transform.localPosition = targetPosition;
-            StartCoroutine(StartPositionChangeCooldown());
-        } 
-        
-    }
-
-    IEnumerator StartPositionChangeCooldown() {
-        canChangePositions = false;
-        yield return new WaitForSecondsRealtime(changePositionCooldownMS / 1000);
-        canChangePositions = true;
     }
 }
