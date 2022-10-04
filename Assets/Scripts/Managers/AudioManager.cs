d44dced802b3edcdf807c12e72fae075d9127645
Rasmus Skovbo <rasmus.skovbo@shape.dk>
2022-10-04 14:51:40 +0200

    Added audio managers and files

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 using System;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : Singleton<AudioManager>
 {
+    // Background music for menus
+    // SFX for menus
+    private void Awake()
+    {
+        GetComponent<AudioSource>().Play();
+        DontDestroyOnLoad(gameObject);
+    }
     
 }
--- Assets/Scripts/Mini Games/ServeRight/Managers/ServeRightSFXManager.cs
+++ Assets/Scripts/Mini Games/ServeRight/Managers/ServeRightSFXManager.cs
@@ -3,5 +3,91 @@ using System;
 
 public class ServeRightSFXManager : MonoBehaviour
 {
+    #region SFX Files
+    [Header("SFX audio clips")]
+    public AudioClip itemPickedUpSFX;
+    public AudioClip itemDeliveredSFX;
+    public AudioClip orderCompletedSFX;
+    public AudioClip orderExpiredSFX;
+    public AudioClip playShaperSpawnedSFX;
+
+    [Header("Audio Settings")] 
+    public float sfxVolume = 1;
+    #endregion
+
+    #region Private Variables
+    private OrderManager orderManager;
+    private BeerSpawner beerSpawner;
+    private BurgerSpawner burgerSpawner;
+    private ShapersSpawner shaperSpawner;
+    #endregion
+    
+    public bool isAudioEnabled = true;
+    
+    private void Start()
+    {
+        orderManager = OrderManager.Instance;
+        beerSpawner = BeerSpawner.Instance;
+        burgerSpawner = BurgerSpawner.Instance;
+        shaperSpawner = ShapersSpawner.Instance;
+
+        orderManager.OnOrderExpired += PlayOrderExpiredSFX;
+        beerSpawner.OnBeerPickedUp += PlayItemPickedUpSFX;
+        burgerSpawner.OnBurgerPickedUp += PlayItemPickedUpSFX;
+        shaperSpawner.OnItemDelivered += PlayItemDeliveredSFX;
+        shaperSpawner.OnOrderDelivered += PlayOrderCompletedSFX;
+        shaperSpawner.OnOrderSpawned += PlayShaperSpawnedSFX;
+
+    }
+
+    #region SFX functions
+    private void PlayItemPickedUpSFX(bool wasAdded)
+    {
+        PlaySFX(itemPickedUpSFX);
+    }
+
+    private void PlayOrderExpiredSFX(int orderId)
+    {
+        PlaySFX(orderExpiredSFX);
+    }
+    
+    private void PlayItemDeliveredSFX(ItemType type)
+    {
+        PlaySFX(itemDeliveredSFX);
+    }
+    
+    private void PlayOrderCompletedSFX(int orderId)
+    {
+        PlaySFX(orderCompletedSFX);
+    }
+    
+    private void PlayShaperSpawnedSFX(int orderId, Shaper shaper)
+    {
+        PlaySFX(playShaperSpawnedSFX);
+    }
+    #endregion
+
+    #region Supporting functions
+
+    public void ToggleAudio()
+    {
+        isAudioEnabled = !isAudioEnabled;
+    }
+    
+    private void PlaySFX(AudioClip sfxClip)
+    {
+        if (isAudioEnabled)
+        {
+            AudioSource.PlayClipAtPoint(
+                sfxClip,
+                Camera.main.transform.position,
+                sfxVolume
+            );
+        }
+    }
+
+    #endregion
+     
+    
     
 }
